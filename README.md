86e3204e8340068fdfb70889ae89bacfa9efb431
sampyle03 <sampyle@outlook.com>
2025-03-12 17:04:45 +0000

    commit-march-12

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 1. Run "npm install"
 =======
 Front-End: React.js, html, css
-Back-End: JavaScript, Python
+Back-End: JavaScript, Python (v3.12)
 
 1. Run "npm install"
 >>>>>>> f9b047e (ml-model-begin)
--- backend/model/data_processor.py
+++ backend/model/data_processor.py
@@ -8,25 +8,94 @@ Returns: None
 import pandas as pd
 import os
 import csv
+from nltk.corpus import stopwords
+import regex as re
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from scipy.sparse import csr_matrix
 
-def process_data(data_path):
-    # Load data from json file
+def clean_text(text):
+    """
+    Function: Cleans given text of stopwords and punctuation, and then lemmatizes the text
+    Parameters: text (str)
+    Returns: cleaned tokens
+    """
+    cleaned_tokens = []
+    cleaned_text = re.sub(r"[^\w\s']+", " ", text) # https://stackoverflow.com/questions/31191986/br-tag-screwing-up-my-data-from-scraping-using-beautiful-soup-and-python
+    tokens = word_tokenize(cleaned_text) # tokenizes text
+    lemmatizer = WordNetLemmatizer()
+    for t in tokens:
+        if t not in set(stopwords.words('english')): # prevents stopwords from being tokenized
+            cleaned_tokens.append(lemmatizer.lemmatize(t.lower()))
+            #cleaned_tokens.append(ps.stem(t)) # can uncomment to use stemming
+    return cleaned_tokens
+
+def load_data(data_path):
+    """
+    Function: Loads all valid data entried and saves it to a pkl file
+    Parameters: None
+    Returns: words
+    """
+
+    # Load data from tsv file
     chosen_columns = ["id", "label", "statement", "subjects", "speaker", "speaker_job_title", "state_info", "party_affiliation","barely_true_counts", "false_counts", "half_true_counts", "mostly_true_counts", "pants_on_fire_counts", "context"]
-    unprocessed_data = pd.DataFrame(columns=chosen_columns)
-    with open(data_path) as fd:
+    loaded_data = pd.DataFrame(columns=chosen_columns)
+    with open(data_path, encoding="utf8") as fd:
         rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        possible_ratings = ["true", "mostly-true", "half-true", "barely-true", "false", "pants-fire"]
+        words = []
         for row in rd:
-            unprocessed_data = pd.concat([unprocessed_data, pd.DataFrame([row], columns=chosen_columns)], ignore_index=True)
-            print(unprocessed_data)
-            input("Press Enter to continue...")
 
-    # Drop unnecessary columns
-"""    dropped_columns = unprocessed_data.drop(columns=['statement_date', "statement_source", "factcheck_date", "factcheck_analysis_link"])
-    print(dropped_columns.columns)
-    input("Press Enter to continue...")"""
+            # Exclude rows with incomplete data
+            if (len(row) == 14):
 
+                # Exclude rows with incorrect data types (NOT WORKING, row[8] - row[12] isinstance str)
+                if ((isinstance(row[0], str)) and (isinstance(row[1], str)) and (row[1] in possible_ratings)
+                and isinstance(row[2], str) and isinstance(row[3], str) and isinstance(row[4], str)
+                and isinstance(row[5], str) and isinstance(row[6], str) and isinstance(row[7], str)
+                and row[8].isnumeric() and row[9].isnumeric() and row[10].isnumeric()
+                and row[11].isnumeric() and row[12].isnumeric() and isinstance(row[13], str)):
+                    
+                    #adds row to pandas table
+                    loaded_data = pd.concat([loaded_data, pd.DataFrame([row], columns=chosen_columns)], ignore_index=True)
+
+    loaded_data.to_pickle(os.path.join(current_dir,'../data/data.pkl'))
+    input("- Data Loaded\n- Press Enter")
+
+def process_data():
+    """
+    Function: Loads data from pkl file, processes it and saves it to another pkl file
+    Parameters: None
+    Returns: None
+    """
+
+    features = pd.DataFrame()
+
+    # Load data from pkl file
+    print("Loading pkl file...")
+    unprocessed_data = pd.read_pickle(os.path.join(current_dir,'../data/data.pkl'))
+
+    # Process data
+
+    words = []
+    os.system('cls')
+    print("Tokenizing statements...")
+    unprocessed_data['statement'] = unprocessed_data['statement'].apply(clean_text)
+
+    # keep track of words used in statements
+    os.system('cls')
+    print("Generating word list...")
+    for statement in unprocessed_data['statement']:
+        for word in statement:
+            if word not in words:
+                words.append(word)
+    
+    print(len(words))
+    print(unprocessed_data['statement'].head())
+    input("- Press Enter")
 
 
 current_dir = os.path.dirname(__file__)
-data_path = os.path.join(current_dir, "../data/train.tsv")
-process_data(data_path)
+data_path = os.path.join(current_dir, "../data/train.tsv") #LIAR dataset
+#load_data(data_path)
+process_data()
